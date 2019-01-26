0acfcfea60aa6a08463e84c38ef4470626cfe559
sami <kozlosam@gmail.com>
2019-01-26 19:23:26 +0000

    Implemented Canvas iterator
    Test for default colour now passes

--- src/main/java/io/raytracer/drawing/PPMCanvas.java
+++ src/main/java/io/raytracer/drawing/PPMCanvas.java
@@ -3,11 +3,43 @@ package io.raytracer.drawing;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class PPMCanvas implements Canvas {
     private List<List<Colour>> pixelGrid;
+    static private final Colour initialColour = new Unit3TupleColour(0 ,0,0);
 
-    private static Colour initialColour = new Unit3TupleColour(0 ,0,0);
+    private class PPMCanvasIterator implements Iterator<Colour> {
+        private Iterator<Colour> horizontalIterator;
+        private Iterator<List<Colour>> verticalIterator;
+
+        PPMCanvasIterator() {
+            verticalIterator = pixelGrid.iterator();
+            horizontalIterator = verticalIterator.next().iterator();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return horizontalIterator.hasNext() || verticalIterator.hasNext();
+        }
+
+        @Override
+        public Colour next() throws NoSuchElementException {
+            if (horizontalIterator.hasNext()) {
+                return horizontalIterator.next();
+            } else if (verticalIterator.hasNext()) {
+                horizontalIterator = verticalIterator.next().iterator();
+                return horizontalIterator.next();
+            } else {
+                throw new NoSuchElementException();
+            }
+        }
+    }
+
+    @Override
+    public Iterator<Colour> iterator() {
+        return new PPMCanvasIterator();
+    }
 
     public PPMCanvas(int x, int y) {
         pixelGrid = Collections.nCopies(y, Collections.nCopies(x, initialColour));
@@ -16,11 +48,6 @@ public class PPMCanvas implements Canvas {
     @Override
     public void write(int x, int y, Colour colour) {
 
-    }
-
-    @Override
-    public Iterator<Colour> iterator() {
-        return null;
     }
 
     @Override
