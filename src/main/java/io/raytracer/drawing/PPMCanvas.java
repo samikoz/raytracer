c68b7e54e0267ccab94f8907b698e35251005f2c
sami <kozlosam@gmail.com>
2019-01-27 13:23:22 +0000

    Refactor of PPMCanvas iterator
    Changed from lists to arrays to allow for writing to them

--- src/main/java/io/raytracer/drawing/PPMCanvas.java
+++ src/main/java/io/raytracer/drawing/PPMCanvas.java
@@ -1,35 +1,36 @@
 package io.raytracer.drawing;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Iterator;
-import java.util.List;
 import java.util.NoSuchElementException;
 
 public class PPMCanvas implements Canvas {
-    private List<List<Colour>> pixelGrid;
+    private Colour[][] pixelGrid;
     static private final Colour initialColour = new Unit3TupleColour(0 ,0,0);
 
     private class PPMCanvasIterator implements Iterator<Colour> {
-        private Iterator<Colour> horizontalIterator;
-        private Iterator<List<Colour>> verticalIterator;
+        private int horizontalIndex;
+        private int verticalIndex;
 
         PPMCanvasIterator() {
-            verticalIterator = pixelGrid.iterator();
-            horizontalIterator = verticalIterator.next().iterator();
+            verticalIndex = 0;
+            horizontalIndex = 0;
         }
 
         @Override
         public boolean hasNext() {
-            return horizontalIterator.hasNext() || verticalIterator.hasNext();
+            return verticalIndex < pixelGrid.length - 1 || horizontalIndex < pixelGrid[verticalIndex].length;
         }
 
         @Override
         public Colour next() throws NoSuchElementException {
-            if (horizontalIterator.hasNext()) {
-                return horizontalIterator.next();
-            } else if (verticalIterator.hasNext()) {
-                horizontalIterator = verticalIterator.next().iterator();
-                return horizontalIterator.next();
+            if (hasNext()) {
+                if (horizontalIndex < pixelGrid[verticalIndex].length) {
+                    return pixelGrid[verticalIndex][horizontalIndex++];
+                } else {
+                    horizontalIndex = 0;
+                    return pixelGrid[++verticalIndex][horizontalIndex];
+                }
             } else {
                 throw new NoSuchElementException();
             }
@@ -42,7 +43,10 @@ public class PPMCanvas implements Canvas {
     }
 
     public PPMCanvas(int x, int y) {
-        pixelGrid = Collections.nCopies(y, Collections.nCopies(x, initialColour));
+        pixelGrid = new Colour[y][x];
+        for (int i = 0; i < y; i++) {
+            Arrays.fill(pixelGrid[i], initialColour);
+        }
     }
 
     @Override
