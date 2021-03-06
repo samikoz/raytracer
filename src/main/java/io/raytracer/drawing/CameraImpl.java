3d7d2abf71de41e3ca81496645d9c1a329790d91
sami <kozlosam@gmail.com>
2021-03-06 22:58:40 +0100

    Camera
    computing pixel size
    transformation getter

--- src/main/java/io/raytracer/drawing/CameraImpl.java
+++ src/main/java/io/raytracer/drawing/CameraImpl.java
@@ -3,12 +3,38 @@ package io.raytracer.drawing;
 import io.raytracer.geometry.ThreeTransformation;
 
 public class CameraImpl implements Camera {
-    public CameraImpl(int hsize, int vsize, double fieldOfView) {
+    private final int hsize;
+    private final int vsize;
+    private final double fieldOfView;
+    private final ThreeTransformation transformation;
+    private double halfWidth;
+    private double halfHeight;
+    private double pixelSize;
 
+    public CameraImpl(int hsize, int vsize, double fieldOfView) {
+        this.hsize = hsize;
+        this.vsize = vsize;
+        this.fieldOfView = fieldOfView;
+        this.transformation = new ThreeTransformation();
     }
 
     @Override
     public ThreeTransformation getTransformation() {
-        return null;
+        return transformation;
+    }
+
+    private void computePixelSize() {
+        double halfView = Math.tan(fieldOfView / 2);
+        double ratio = (double) hsize / vsize;
+
+        if (ratio >= 1) {
+            halfWidth = halfView;
+            halfHeight = halfView / ratio;
+        } else {
+            halfWidth = halfView * ratio;
+            halfHeight = halfView;
+        }
+
+        pixelSize = halfWidth * 2 / hsize;
     }
 }
