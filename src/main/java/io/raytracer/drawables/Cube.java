10b9ec95821be090d792c2383276b18191e92fc7
sami <kozlosam@gmail.com>
2023-04-04 18:16:14 +0200

    Cubes
    localIntersections
    localNormals

---
+++ src/main/java/io/raytracer/drawables/Cube.java
@@ -0,0 +1,52 @@
+package io.raytracer.drawables;
+
+import io.raytracer.geometry.IPoint;
+import io.raytracer.geometry.IVector;
+import io.raytracer.geometry.Vector;
+import io.raytracer.materials.Material;
+import io.raytracer.mechanics.IRay;
+import lombok.NonNull;
+
+public class Cube extends Drawable {
+    Cube() {
+        super();
+    }
+
+    Cube(@NonNull Material material) {
+        super(material);
+    }
+
+    @Override
+    protected double[] getLocalIntersectionPositions(IRay ray) {
+        double[] xIntersections = this.getAxisIntersections(ray.getOrigin().get(0), ray.getDirection().get(0));
+        double[] yIntersections = this.getAxisIntersections(ray.getOrigin().get(1), ray.getDirection().get(1));
+        double[] zIntersections = this.getAxisIntersections(ray.getOrigin().get(2), ray.getDirection().get(2));
+
+        double maxMinIntersection = Math.max(Math.max(xIntersections[0], yIntersections[0]), zIntersections[0]);
+        double minMaxIntersection = Math.min(Math.min(xIntersections[1], yIntersections[1]), zIntersections[1]);
+
+        if (maxMinIntersection > minMaxIntersection) {
+            return new double[] {};
+        }
+
+        return new double[] { maxMinIntersection, minMaxIntersection };
+    }
+
+    private double[] getAxisIntersections(double originComponent, double directionComponent) {
+        double oneIntersection = (-1 - originComponent)/directionComponent;
+        double otherIntersection = (1 - originComponent)/directionComponent;
+        return new double[] {Math.min(oneIntersection, otherIntersection), Math.max(oneIntersection, otherIntersection)};
+    }
+
+    @Override
+    protected IVector normalLocally(IPoint point) {
+        double maxComponent = Math.max(Math.abs(point.get(0)), Math.max(Math.abs(point.get(1)), Math.abs(point.get(2))));
+
+        if (maxComponent == Math.abs(point.get(0))) {
+            return new Vector(point.get(0), 0, 0);
+        } else if (maxComponent == Math.abs(point.get(1))) {
+            return new Vector(0, point.get(1), 0);
+        }
+        return new Vector(0, 0, point.get(2));
+    }
+}
---
+++ src/test/java/io/raytracer/drawables/CubeTest.java
@@ -0,0 +1,78 @@
+package io.raytracer.drawables;
+
+import io.raytracer.geometry.IVector;
+import io.raytracer.geometry.Point;
+import io.raytracer.geometry.Vector;
+import io.raytracer.mechanics.Ray;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CubeTest {
+    private static Stream<Arguments> providePositiveRaysAndIntersections() {
+        return Stream.of(
+                Arguments.of(new Ray(new Point(5, 0.5, 0), new Vector(-1,0,0)), 4.0, 6.0),
+                Arguments.of(new Ray(new Point(-5, 0.5, 0), new Vector(1,0,0)), 4.0, 6.0),
+                Arguments.of(new Ray(new Point(0.5, 5, 0), new Vector(0,-1,0)), 4.0, 6.0),
+                Arguments.of(new Ray(new Point(0.5, -5, 0), new Vector(0,1,0)), 4.0, 6.0),
+                Arguments.of(new Ray(new Point(0.5, 0, 5), new Vector(0,0,-1)), 4.0, 6.0),
+                Arguments.of(new Ray(new Point(0.5, 0, -5), new Vector(0,0,1)), 4.0, 6.0),
+                Arguments.of(new Ray(new Point(0, 0.5, 0), new Vector(0,0,1)), -1, 1)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("providePositiveRaysAndIntersections")
+    void positiveLocalIntersectionPositions(Ray ray, double firstIntersection, double secondIntersection) {
+        Drawable cube = new Cube();
+        double[] intersections = cube.getLocalIntersectionPositions(ray);
+        assertEquals(2, intersections.length);
+        assertEquals(firstIntersection, intersections[0], 1e-3);
+        assertEquals(secondIntersection, intersections[1], 1e-3);
+    }
+
+    private static Stream<Arguments> provideMissingRaysAndIntersections() {
+        return Stream.of(
+                Arguments.of(new Ray(new Point(2, 0, 0), new Vector(0.2673, 0.5345, 0.8018))),
+                Arguments.of(new Ray(new Point(0, -2, 0), new Vector(0.8018, 0.2673, 0.5345))),
+                Arguments.of(new Ray(new Point(0, 0, -2), new Vector(0.5345, 0.8018, 0.2673))),
+                Arguments.of(new Ray(new Point(2, 0, 2), new Vector(0, 0, -1))),
+                Arguments.of(new Ray(new Point(0, 2, 2), new Vector(0, -1, 0))),
+                Arguments.of(new Ray(new Point(2, 2, 0), new Vector(1, 0, 0)))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideMissingRaysAndIntersections")
+    void missingLocalIntersectionPositions(Ray ray) {
+        Drawable cube = new Cube();
+        double[] intersections = cube.getLocalIntersectionPositions(ray);
+        assertEquals(0, intersections.length);
+    }
+
+    private static Stream<Arguments> providePointsAndNormals() {
+        return Stream.of(
+                Arguments.of(new Point(1, 0.5, -0.8), new Vector(1, 0, 0)),
+                Arguments.of(new Point(-1, -0.2, 0.9), new Vector(-1, 0, 0)),
+                Arguments.of(new Point(-0.4, 1, -0.1), new Vector(0, 1, 0)),
+                Arguments.of(new Point(0.3, -1, -0.7), new Vector(0, -1, 0)),
+                Arguments.of(new Point(0.6, 0.3, 1), new Vector(0, 0, 1)),
+                Arguments.of(new Point(0.4, 0.4, -1), new Vector(0, 0, -1)),
+                Arguments.of(new Point(1, 1, 1), new Vector(1, 0, 0)),
+                Arguments.of(new Point(-1, -1, -1), new Vector(-1, 0, 0))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("providePointsAndNormals")
+    void normalLocally(Point point, Vector normal) {
+        Drawable cube = new Cube();
+        IVector actualNormal = cube.normalLocally(point);
+        assertEquals(normal, actualNormal);
+    }
+}
