fa8f8850db213f2798100e5b95c3d20aed742a33
sami <kozlosam@gmail.com>
2023-07-24 16:20:23 +0200

    Helper textures

---
+++ src/main/java/io/raytracer/textures/Textures.java
@@ -0,0 +1,8 @@
+package io.raytracer.textures;
+
+import io.raytracer.tools.LinearColour;
+
+public class Textures {
+    public static Texture Black = new MonocolourTexture(new LinearColour(0, 0, 0));
+    public static Texture White = new MonocolourTexture(new LinearColour(1, 1, 1));
+}
