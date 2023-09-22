34eaf2723c9a8dcb8520cad8db9cec3f185976a5
sami <kozlosam@gmail.com>
2023-09-22 21:41:39 +0200

    Cages demos

--- src/main/java/io/raytracer/demos/Cage.java
+++ src/main/java/io/raytracer/demos/Cage.java
@@ -10,8 +10,8 @@ import io.raytracer.mechanics.IRay;
 import io.raytracer.mechanics.LambertianWorld;
 import io.raytracer.mechanics.Recasters;
 import io.raytracer.shapes.Cylinder;
+import io.raytracer.shapes.Disc;
 import io.raytracer.shapes.Group;
-import io.raytracer.shapes.Plane;
 import io.raytracer.shapes.Shape;
 import io.raytracer.shapes.Sphere;
 import io.raytracer.textures.MonocolourTexture;
@@ -30,19 +30,20 @@ import java.util.function.Function;
 public class Cage {
     public static void main(String[] args) throws IOException {
         int firstBarPosition = -16;
-        int barCount = 7;
+        int barCount = 5;
         double barSeparation = 3;
         double barScale = 0.5;
+        int raysCount = Integer.parseInt(args[0]);
 
         Material floorMaterial = Material.builder()
                 .texture(new MonocolourTexture(new LinearColour(0.2, 0.2, 0.2)))
                 .build();
         floorMaterial.addRecaster(Recasters.diffuse, 1);
-        Shape floor = new Plane(floorMaterial);
+        Shape floor = new Disc(floorMaterial);
+        floor.setTransform(ThreeTransform.rotation_x(Math.PI / 2).scale(27, 27, 27).translate(barSeparation*(barCount-1)/2.0, 0, firstBarPosition + (barSeparation*barCount-1)/2.0));
 
         Function<IRay, IColour> background = ray -> new LinearColour(0, 0, 0);
         LambertianWorld world = new LambertianWorld(background);
-        world.put(floor);
 
         Material barMaterial = Material.builder()
                 .texture(new MonocolourTexture(new LinearColour(0.2, 0.2, 0.2)))
@@ -57,10 +58,14 @@ public class Cage {
         for (int barIndex = 0; barIndex < barCount; barIndex++) {
             double barZPosition = firstBarPosition + barSeparation*barIndex;
             double barXPosition = -firstBarPosition + barZPosition;
-            Shape backBar = new Cylinder(barMaterial);
-            Shape rightBar = new Cylinder(barMaterial);
-            Shape frontBar = new Cylinder(barMaterial);
-            Shape leftBar = new Cylinder(barMaterial);
+            Cylinder backBar = new Cylinder(barMaterial);
+            backBar.setLowerBound(0);
+            Cylinder rightBar = new Cylinder(barMaterial);
+            rightBar.setLowerBound(0);
+            Cylinder frontBar = new Cylinder(barMaterial);
+            frontBar.setLowerBound(0);
+            Cylinder leftBar = new Cylinder(barMaterial);
+            leftBar.setLowerBound(0);
             backBar.setTransform(ThreeTransform.scaling(barScale, 1, barScale).translate(0, 0, barZPosition));
             backGroup.add(backBar);
             frontBar.setTransform(ThreeTransform.scaling(barScale, 1, barScale).translate(-firstBarPosition + barEndPosition, 0, barZPosition));
@@ -72,7 +77,6 @@ public class Cage {
                 leftGroup.add(leftBar);
             }
         }
-        world.put(backGroup).put(rightGroup).put(frontGroup).put(leftGroup);
 
         /*
         Material smokeMaterial = Material.builder().texture(new MonocolourTexture(new LinearColour(0.8, 0.8, 0.8))).build();
@@ -83,17 +87,23 @@ public class Cage {
         world.put(smoke1);
         */
 
-        Material emitentMaterial = Material.builder().emit(new LinearColour(60, 60, 60)).build();
+        Material emitentMaterial = Material.builder().emit(new LinearColour(40, 40, 40)).build();
         Shape emitent = new Sphere(emitentMaterial);
-        emitent.setTransform(ThreeTransform.scaling(7, 7, 7).translate(barSeparation*(barCount-1)/2.0, 30, firstBarPosition + (barSeparation*barCount-1)/2.0));
-        world.put(emitent);
+        emitent.setTransform(ThreeTransform.scaling(4, 4, 4).translate(barSeparation*(barCount-1)/2.0, 30, firstBarPosition + (barSeparation*barCount-1)/2.0));
 
-        IPoint eyePosition = new Point(45, 12, 33.915974);
-        IVector lookDirection = new Point(10, 6, -5).subtract(eyePosition);
+        Group all = new Group();
+        all.setTransform(ThreeTransform.translation(0, 12, 0));
+        all.add(backGroup).add(leftGroup).add(frontGroup).add(rightGroup).add(floor).add(emitent);
+        world.put(all);
+
+        IPoint eyePosition = new Point(30, 15, 17.915974).multiply(2);
+        IVector lookDirection = new Vector(-35, -18, -39);
         IVector upDirection = new Vector(0, 1, 0);
-        Camera camera = new MultipleRayCamera(500, 1024, 768, Math.PI / 4, eyePosition, lookDirection, upDirection);
+        Camera camera = new MultipleRayCamera(raysCount, 1080, 1080, Math.PI / 4, eyePosition, lookDirection, upDirection);
 
-        String filename = "cage.ppm";
+        //put RadialGradient for the disc's texture, maybe adjust it so that in the middle square the color is constant
+        //lower everything so that when we add white stripes the effect is as the present one
+        String filename = String.format("cage_%3d.ppm", raysCount);
 
         PrintWriter writer = new PrintWriter(new FileWriter(filename));
         IPicture picture = world.render(camera);
---
+++ src/main/java/io/raytracer/shapes/Disc.java
@@ -0,0 +1,46 @@
+package io.raytracer.shapes;
+
+import io.raytracer.geometry.IPoint;
+import io.raytracer.geometry.IVector;
+import io.raytracer.geometry.Point;
+import io.raytracer.geometry.Vector;
+import io.raytracer.materials.Material;
+import io.raytracer.mechanics.BBox;
+import io.raytracer.mechanics.IRay;
+import io.raytracer.mechanics.Intersection;
+
+import java.util.Optional;
+
+public class Disc extends Shape {
+    public Disc() {
+        super();
+    }
+
+    public Disc(Material material) {
+        super(material);
+    }
+
+    @Override
+    protected Intersection[] getLocalIntersections(IRay ray, double tmin, double tmax) {
+        if (ray.getDirection().get(2) == 0) {
+            return new Intersection[] {};
+        }
+        double positionAtZZero = -ray.getOrigin().get(2)/ray.getDirection().get(2);
+        double xCoordAtZZero = ray.getOrigin().get(0) + positionAtZZero*ray.getDirection().get(0);
+        double yCoordAtZZero = ray.getOrigin().get(1) + positionAtZZero*ray.getDirection().get(1);
+        if (Math.pow(xCoordAtZZero,2) + Math.pow(yCoordAtZZero,2) < 1 && positionAtZZero >= tmin && positionAtZZero <= tmax) {
+            return new Intersection[] { new Intersection(this, ray, positionAtZZero, 0, 0) };
+        }
+        return new Intersection[] {};
+    }
+
+    @Override
+    protected IVector localNormalAt(IPoint point, double u, double v) {
+        return new Vector(0, 0, -1);
+    }
+
+    @Override
+    protected Optional<BBox> getLocalBoundingBox() {
+        return Optional.of(new BBox(new Point(0, -1e-3, 0), new Point(1, 1e-3, 1)));
+    }
+}
